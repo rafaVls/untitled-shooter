e9e17c4eb54c033e2a67828bab7a51f214333915
Dan Ewing <dsewing22@gmail.com>
2022-01-19 22:11:22 -0800

    Working spawner with enemy follow, player child object facing mouse, etc
    *Placeholder sprites for gun and bullet objects rn

---
+++ Untitled_Shooter/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform[] spawnSpots;
+    public float timeBetweenSpawns;
+    public float startTimeBetweenSpawns;
+
+    void Start() 
+    {
+        timeBetweenSpawns = startTimeBetweenSpawns;
+    }
+
+    void Update() 
+    {
+        if(timeBetweenSpawns <= 0) 
+        {
+            int randomPosition = Random.Range(0, spawnSpots.Length - 1);
+            Instantiate(enemy, spawnSpots[randomPosition].position, Quaternion.identity);
+            timeBetweenSpawns = startTimeBetweenSpawns;
+        }
+        else 
+            {
+                timeBetweenSpawns -= Time.deltaTime;
+            }
+    }
+
+}
