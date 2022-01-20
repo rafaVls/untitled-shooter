7b830f96990018ce50f45ef2f0666e4d39fd7fc2
Rafael Avilés <rafa.vfierro@outlook.com>
2022-01-20 11:25:48 -0800

    add comments to FaceMouse
    also add xAxis variable for clarity

--- Untitled_Shooter/Assets/Scripts/FaceMouse.cs
+++ Untitled_Shooter/Assets/Scripts/FaceMouse.cs
@@ -6,8 +6,14 @@ public class FaceMouse : MonoBehaviour
 {
     void Update()
     {
-        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        // Getting the point in the screen where the mouse is located
+        Vector3 mousePos = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+
+        // Getting the angle between the x axis and the mouse position
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        // Getting the x axis and actually rotating the object
+        Vector3 xAxis = Vector3.forward;
+        transform.rotation = Quaternion.AngleAxis(angle, xAxis);
     }
 }
