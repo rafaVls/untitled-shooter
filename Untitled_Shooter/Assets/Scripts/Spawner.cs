b7550889958f1b3b2035174241e27bef03f2acf3
Rafael Avilés <rafa.vfierro@outlook.com>
2022-01-20 22:07:59 -0800

    fix spawner gameObject spawning enemies
    this fixes #4 (hopefully)

--- Untitled_Shooter/Assets/Scripts/Spawner.cs
+++ Untitled_Shooter/Assets/Scripts/Spawner.cs
@@ -7,11 +7,13 @@ public class Spawner : MonoBehaviour
     public GameObject enemy;
     public float timeBetweenSpawns;
     public float startTimeBetweenSpawns;
-    Transform[] spawnSpots;
+    List<Transform> spawnSpots = new();
 
     void Start() 
     {
-        spawnSpots = GetComponentsInChildren<Transform>();
+        // Adding all children and removing the spawner (which gets added for some reason)
+        spawnSpots.AddRange(GetComponentsInChildren<Transform>());
+        spawnSpots.Remove(spawnSpots[0]);
         timeBetweenSpawns = startTimeBetweenSpawns;
     }
 
@@ -19,7 +21,7 @@ public class Spawner : MonoBehaviour
     {
         if(timeBetweenSpawns <= 0) 
         {
-            int randomPosition = Random.Range(0, spawnSpots.Length - 1);
+            int randomPosition = Random.Range(0, spawnSpots.Count - 1);
             Instantiate(enemy, spawnSpots[randomPosition].position, Quaternion.identity);
             timeBetweenSpawns = startTimeBetweenSpawns;
         }
