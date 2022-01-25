c1f2edd20f19a3c8f48a765f60a933283e607c5c
Rafael Avilés <rafa.vfierro@outlook.com>
2022-01-25 12:41:44 -0800

    feat: add controller for tying animations to player

--- Untitled_Shooter/Assets/Scripts/Player.cs
+++ Untitled_Shooter/Assets/Scripts/Player.cs
@@ -7,11 +7,15 @@ public class Player : MonoBehaviour
     public float speed;
     Rigidbody2D rb;
     Vector2 moveVelocity;
+    Animator animator;
+    SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {        
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,10 +26,31 @@ public class Player : MonoBehaviour
         Vector2 moveInput = new Vector2(x, y);
         
         moveVelocity = moveInput.normalized * speed;
+        triggerRunAnimation(x, y);
+        flipSprite(x);
     }
 
     void FixedUpdate() 
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);    
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    void triggerRunAnimation(float hor, float vert)
+    {
+        float playerDir = hor == 0 ? vert : hor;
+        animator.SetFloat("Speed", Mathf.Abs(playerDir * speed));
+    }
+
+    // investigate if this function can be simplified, I don't like if elses
+    void flipSprite(float horizontalDir)
+    {
+        if (horizontalDir > 0) 
+        {
+            sr.flipX = false;
+        }
+        else if (horizontalDir < 0)
+        {
+            sr.flipX = true;
+        }
     }
 }
