0d0152d01c29a23f910fc9c04121fc0ceb6cb4d1
Rafael Avilés <rafa.vfierro@outlook.com>
2022-02-03 23:28:17 -0800

    feat: add spawner functionality (possibly final)

--- Untitled_Shooter/Assets/Scripts/Spawner.cs
+++ Untitled_Shooter/Assets/Scripts/Spawner.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject enemy;
-    public float timeBetweenSpawns;
     public float startTimeBetweenSpawns;
+    public float timeBetweenSpawns;
+    public int numberOfEnemies = 10;
+
+    public List<GameObject> enemyTypes;
     List<Transform> spawnSpots = new();
+    List<Enemy> enemyScripts = new();
+    List<GameObject> enemies = new();
 
     void Start() 
     {
@@ -15,19 +19,56 @@ public class Spawner : MonoBehaviour
         spawnSpots.AddRange(GetComponentsInChildren<Transform>());
         spawnSpots.Remove(spawnSpots[0]);
         timeBetweenSpawns = startTimeBetweenSpawns;
+
+        foreach (GameObject type in enemyTypes)
+        {
+            Enemy script = type.GetComponent<Enemy>();
+            enemyScripts.Add(script);
+        }
+
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            int randomNumber = CalculateSpawn(enemyScripts);
+            enemies.Add(enemyTypes[randomNumber]);
+        }
     }
 
     void Update() 
     {
-        if(timeBetweenSpawns <= 0) 
-        {
-            int randomPosition = Random.Range(0, spawnSpots.Count - 1);
-            Instantiate(enemy, spawnSpots[randomPosition].position, Quaternion.identity);
-            timeBetweenSpawns = startTimeBetweenSpawns;
-        }
-        else 
+        if (enemies.Count <= 0) return;
+        if (timeBetweenSpawns > 0)
         {
             timeBetweenSpawns -= Time.deltaTime;
+            return;
         }
+
+        int randomPosition = Random.Range(0, spawnSpots.Count - 1);
+        int randomIndex = Random.Range(0, enemies.Count);
+
+        Instantiate(enemies[randomIndex], spawnSpots[randomPosition].position, Quaternion.identity);
+        enemies.Remove(enemies[randomIndex]);
+        
+        timeBetweenSpawns = startTimeBetweenSpawns;
+    }
+    
+    int CalculateSpawn(List<Enemy> enemyList)
+    {
+        float total = 0;
+
+        foreach (Enemy enemy in enemyList)
+        {
+            total += enemy.spawnChance;
+        }
+
+        float randomPoint = Random.value * total;
+
+        for (int i = 0; i < enemyList.Count; i++)
+        {
+            float sw = enemyScripts[i].spawnChance;
+
+            if (randomPoint < sw) return i;
+            else randomPoint = sw;
+        }
+        return enemyTypes.Count - 1;
     }
 }
